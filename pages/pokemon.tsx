e584da122833bdbe1b14efa09d9d7e5b6e599085
dvrepa <d.v.repnikov@gmail.com>
2022-07-21 20:16:25 +0300

    pokemons task

---
+++ pages/pokemon.tsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+import { NextPage } from "next";
+import Pokemons from "../assets/pokemons.json";
+import { ChangeEventHandler, useEffect, useRef, useState } from "react";
+
+interface IPokemonPageProps {}
+
+const PokemonPage: NextPage<IPokemonPageProps> = () => {
+  const [pokemons, setPokemons] = useState(Pokemons);
+  const [searchValue, setSearchValue] = useState("");
+
+  const timerRef = useRef<ReturnType<typeof setTimeout>>();
+
+  const onInputChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  // + debouncing
+  useEffect(() => {
+    clearTimeout(timerRef.current);
+
+    timerRef.current = setTimeout(() => {
+      clearTimeout(timerRef.current);
+
+      if (!searchValue.length) {
+        setPokemons(Pokemons);
+      } else {
+        setPokemons(
+          Pokemons.filter((p) => {
+            return p.name.toLowerCase().includes(searchValue.toLowerCase());
+          })
+        );
+      }
+    }, 300);
+  }, [searchValue]);
+
+  return (
+    <>
+      <label>
+        Filter:{" "}
+        <input type={"text"} value={searchValue} onChange={onInputChange} />
+      </label>
+      <ul>
+        {pokemons.map((p) => {
+          return <li key={p.id}>{p.name}</li>;
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default PokemonPage;
